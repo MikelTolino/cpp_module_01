c8cd2801e087fcb5db733806cd8950d679919927
MiguelTolino <mmateo-t@student.42madrid.com>
2022-05-05 17:47:48 +0200

    Ex01 checked

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,20 +6,24 @@
 /*   By: mmateo-t <mmateo-t@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 19:48:20 by mmateo-t          #+#    #+#             */
-/*   Updated: 2022/04/04 12:17:56 by mmateo-t         ###   ########.fr       */
+/*   Updated: 2022/05/05 17:46:30 by mmateo-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
-#define NUM 5
 Zombie* zombieHorde( int N, std::string name );
 
 int	main()
 {
+	size_t num;
 	Zombie *horde;
 
-	horde = zombieHorde(NUM, "Pepitos");
-	for (size_t i = 0; i < NUM; i++)
+	std::cout << "Brainless Zombies: ";
+	std::cin >> num;
+	std::cout << std::endl;
+
+	horde = zombieHorde(num, "Pepitos");
+	for (size_t i = 0; i < num; i++)
 		horde[i].announce();
 	delete [] horde;
 	return (0);
